713141542690821957e992ee58db7a8a015c54d9
Dragan Jeremic <jeremic.dragan1991@gmail.com>
2016-07-20 11:49:40 +0200

    Optimization

--- index.php
+++ index.php
@@ -5,7 +5,6 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Vennvest Financing Solutions</title>
   <!-- Google Fonts -->
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
   <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
