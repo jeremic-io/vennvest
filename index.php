5478defafaf1aa58f545dde01eabad4328cb2a79
Dragan Jeremic <jeremic.dragan1991@gmail.com>
2016-07-20 11:17:37 +0200

    Optimization

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
             <span class="icon-bar"></span>
           </button>
           <a class="navbar-brand" href="index.html">
-            <img src="images/logo.jpg" alt="logo">
+            <img src="images/logo.jpg" alt="logo" width="40" height="40">
             <span>
               VennVest <br>
               Financing Solutions
@@ -45,50 +45,48 @@
   </header>
   <!-- Intro Section -->
   <section class="intro">
-    <div class="intro-overlay">
-      <div class="container">
-        <div class="intro-text">
-          <h1 class="h-1">Real Estate Investment Financing Solutions</h1>
-          <div class="row">
-            <div class="col-xs-12 col-sm-12 col-md-7 col-lg-7">
-              <h2 class="h-2">No Muss No Fuss Loans</h2>
-              <p class="text-justify">
-                Simply put, we thrive on real estate. We lend exclusively to real estate investors, and we are fully capitalized so we are always ready to lend.
-                Because we focus primarily on the value of the asset, our process is quick easy and free of hoop jumping.
-              </p>
-              <p class="text-justify">
-                We offer a variety of products to fit investor needs. We have the product for flipping or building a rental portfolio.
-                With today's interest rates at 5.99% and LTV up to 75% our low and no low doc loans give you options and flexibility as you plan your investmentment strategy.
-              </p>
-              <p class="text-justify">We are committed to excellent customer service and fast, simple loans:</p>
-              <ul>
-                <li>NO income verification</li>
-                <li>NO debt to income calculations</li>
-                <li>Close in weeks not months</li>
-              </ul>
-            </div>
-            <div class="col-xs-12 col-sm-12 col-md-5 col-lg-5">
-              <ul class="side-ul">
-                <li>Rates As Low As 5.99%</li>
-                <li>Buy and Hold Products</li>
-                <li>Flip Products</li>
-                <li>Close in Weeks, not Months</li>
-                <li>Up to 75% LTV on Purchases</li>
-                <li>Up to 75% LTV on Refis</li>
-                <li>Min. Loan Amount: $25K</li>
-                <li>Max. Loan Amount: $2M</li>
-                <li>No Doc & Low Doc</li>
-                <li>Refi While the Home is Listed</li>
-                <li>30-Day Seasoning on Refis</li>
-                <li>630 Min. Credit Score</li>
-                <li>Foreign Nationals Approved</li>
-              </ul>
-            </div>
+    <div class="container">
+      <div class="intro-text">
+        <h1 class="h-1">Real Estate Investment Financing Solutions</h1>
+        <div class="row">
+          <div class="col-xs-12 col-sm-12 col-md-7 col-lg-7">
+            <h2 class="h-2">No Muss No Fuss Loans</h2>
+            <p class="text-justify">
+              Simply put, we thrive on real estate. We lend exclusively to real estate investors, and we are fully capitalized so we are always ready to lend.
+              Because we focus primarily on the value of the asset, our process is quick easy and free of hoop jumping.
+            </p>
+            <p class="text-justify">
+              We offer a variety of products to fit investor needs. We have the product for flipping or building a rental portfolio.
+              With today's interest rates at 5.99% and LTV up to 75% our low and no low doc loans give you options and flexibility as you plan your investmentment strategy.
+            </p>
+            <p class="text-justify">We are committed to excellent customer service and fast, simple loans:</p>
+            <ul>
+              <li>NO income verification</li>
+              <li>NO debt to income calculations</li>
+              <li>Close in weeks not months</li>
+            </ul>
           </div>
-          <div class="row call-to-action">
-            <div class="col-lg-12 text-center">
-              <a href="#" class="btn btn-default call-to-action-btn">Meeting Investors Financing Needs!</a>
-            </div>
+          <div class="col-xs-12 col-sm-12 col-md-5 col-lg-5">
+            <ul class="side-ul">
+              <li>Rates As Low As 5.99%</li>
+              <li>Buy and Hold Products</li>
+              <li>Flip Products</li>
+              <li>Close in Weeks, not Months</li>
+              <li>Up to 75% LTV on Purchases</li>
+              <li>Up to 75% LTV on Refis</li>
+              <li>Min. Loan Amount: $25K</li>
+              <li>Max. Loan Amount: $2M</li>
+              <li>No Doc & Low Doc</li>
+              <li>Refi While the Home is Listed</li>
+              <li>30-Day Seasoning on Refis</li>
+              <li>630 Min. Credit Score</li>
+              <li>Foreign Nationals Approved</li>
+            </ul>
+          </div>
+        </div>
+        <div class="row call-to-action">
+          <div class="col-lg-12 text-center">
+            <a href="#" class="btn btn-default call-to-action-btn">Meeting Investors Financing Needs!</a>
           </div>
         </div>
       </div>
@@ -682,7 +680,7 @@
         </div>
         <div class="col-sm-5 col-md-5 col-lg-5 contact-info">
           <div class="google-maps">
-          <iframe style="border:0" src="https://www.google.com/maps/embed/v1/view?zoom=17&center=40.6892%2C-74.0445&key=AIzaSyD8K10DyEk7qxuH8kYHNPw7jBHii3argKA"></iframe>
+            <iframe style="border:0" src="https://www.google.com/maps/embed/v1/view?zoom=17&center=40.6892%2C-74.0445&key=AIzaSyD8K10DyEk7qxuH8kYHNPw7jBHii3argKA"></iframe>
           </div>
         </div>
       </div>
@@ -702,8 +700,20 @@
     </div>
   </footer>
 
-  <!-- Javascript files -->
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  <!-- defer loading of javascript files -->
+  <script>
+    function downloadJSAtOnload() {
+      var element = document.createElement("script");
+      element.src = "js/scripts.js";
+      document.body.appendChild(element);
+    }
+    if (window.addEventListener) {
+      window.addEventListener("load", downloadJSAtOnload, false);
+    } else if (window.attachEvent) {
+      window.attachEvent("onload", downloadJSAtOnload);
+    } else {
+      window.onload = downloadJSAtOnload;
+    }
+  </script>
 </body>
 </html>
